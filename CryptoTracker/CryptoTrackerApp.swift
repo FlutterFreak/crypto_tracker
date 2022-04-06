e4b16aaa3a438004cfa034ff0dc404df1bcb5835
nirdesh.kulhar <nirdesh.kulhar@publicissapient.com>
2022-04-06 14:16:38 +0530

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Nirdesh Kulhar on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
